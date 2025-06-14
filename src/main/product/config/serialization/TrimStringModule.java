8f3af07e5191872bbeea13f7ece2aa79d9dd8193
thanhdo <billslim0996@gmail.com>
2025-06-14 15:11:43 -0700

    sửa tên constructor, thên constrains vào dto

--- src/main/product/config/serialization/TrimStringModule.java
+++ src/main/product/config/serialization/TrimStringModule.java
@@ -3,7 +3,7 @@ package main.product.config.serialization;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class TrimStringModule extends SimpleModule {
-    public TrimStringModule() {
-        addDeserializer(String.class, new TrimStringDeserializer());
-    }
+	public TrimStringModule() {
+		addDeserializer(String.class, new TrimStringDeserializer());
+	}
 }
