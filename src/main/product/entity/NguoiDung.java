4a8984500c0e85506ff5810d28d7fe8e5a4ec743
thanhdo <billslim0996@gmail.com>
2025-06-15 22:42:37 -0700

    minor debugs

--- src/main/product/entity/NguoiDung.java
+++ src/main/product/entity/NguoiDung.java
@@ -1,6 +1,11 @@
 package main.product.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,11 +16,11 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class NguoiDung {
+public class NguoiDung extends BaseEntity{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(name = "ten_dang_nhap")
+	private Long ma;
+	@Column(name = "ten")
 	private String username;
 	@Column(name = "mat_khau")
 	private String password; // Giả định là đã hash rồi
