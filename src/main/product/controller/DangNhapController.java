f5231b2bcd5c9214098a5ffb40e08324b3d47782
thanhdo <billslim0996@gmail.com>
2025-07-07 10:07:48 -0700

    finalize the login code

--- src/main/product/controller/DangNhapController.java
+++ src/main/product/controller/DangNhapController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,13 @@ public class DangNhapController {
 
 	@Autowired
 	private NguoiDungService service;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@PostMapping
 	public ResponseEntity<?> dangNhap(@RequestBody DangNhapReq req, HttpSession session){
-		Optional<NguoiDungEntity> entity = service.getByUsername(req.getTenNguoiDung());	
-		if (entity.isEmpty() || !entity.get().getPassword().equals(req.getMatKhau()))
+		Optional<NguoiDungEntity> entity = service.getByUsername(req.getTenNguoiDung());
+		if (entity.isEmpty() || !passwordEncoder.matches(req.getMatKhau(), entity.get().getPassword()))
 			return ResponseEntity.status(401).body("Tên đăng nhập hoặc mật khẩu không chính xác");
 		session.setAttribute("username", req.getTenNguoiDung());
 		return ResponseEntity.ok("Thành công");
